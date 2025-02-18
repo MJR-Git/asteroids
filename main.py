d20d504668d4b6e1bbc9e684fe99aaacb694285d
MJR-Git <mayarexen@protonmail.com>
2025-02-18 23:41:19 +0100

    E: more partitioned in functions

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
-pygame.init()
 from constants import *
 
-screen = pygame.display.set_mode((SCREEN_WIDTH , SCREEN_HEIGHT))
-clock = pygame.time.Clock()
-dt = 0
+
 
 def main():
+    pygame.init()
+    screen = get_screen()
+    clock, dt = get_clock()
+
+    start()
+    game_loop(screen, clock, dt)
+
+
+def get_screen():
+    screen = pygame.display.set_mode((SCREEN_WIDTH , SCREEN_HEIGHT))
+    return screen
+
+
+def get_clock():
+    clock = pygame.time.Clock()
+    dt = 0
+    return clock, dt
+
+
+def start():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    game_loop()
-
-
-#def time():
-    #clock = pygame.time.Clock
-    #clock.tick(60)
-    #dt = 0
     
 
-def game_loop():
+def game_loop(screen, clock, dt):
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        clock.tick(60)
-        dt = (clock.tick()) / 1000
+        dt = (clock.tick(60)) / 1000
         pygame.display.flip()
 
     
